c5993f5f6290b00193c0edfb193229621e71aeea
aGlnYWtpbg== <33284075+Higakinn@users.noreply.github.com>
2024-07-11 18:38:33 +0900

    fix get new feed iteams ts

--- src/getNewFeedItems.ts
+++ src/getNewFeedItems.ts
@@ -5,14 +5,14 @@ const parser = new Parser()
 
 export const getNewFeedItems = async (feedUrl: string) => {
   const { items: newFeedItems } = await parser.parseURL(feedUrl)
+  return newFeedItems
+  // return newFeedItems.filter((feedItem) => {
+  //   const { pubDate } = feedItem
 
-  return newFeedItems.filter((feedItem) => {
-    const { pubDate } = feedItem
+  //   if (!pubDate) return false
 
-    if (!pubDate) return false
-
-    const publishedDate = new Date(pubDate).getTime() / 1000
-    const { diffInHours } = timeDifference(publishedDate)
-    return diffInHours === 0
-  })
+  //   const publishedDate = new Date(pubDate).getTime() / 1000
+  //   const { diffInHours } = timeDifference(publishedDate)
+  //   return diffInHours === 0
+  // })
 }
